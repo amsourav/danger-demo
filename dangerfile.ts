378f9d2853550c6f0cd04f4d40002ea80fadf313
sourav sarkar <souravsrkr254@gmail.com>
2021-03-14 13:06:45 +0530

    cleanup

--- dangerfile.ts
+++
@@ -1,6 +0,0 @@
-import { schedule } from 'danger'
-import dependencies from '@seadub/danger-plugin-dependencies'
-import linearHistory from 'danger-plugin-linear-history'
-
-schedule(dependencies())
-linearHistory()
